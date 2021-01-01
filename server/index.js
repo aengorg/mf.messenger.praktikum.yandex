522b462ef095f9f83509ab7e4bf651896622513c
Nikita Stroganov <findoss@yandex.ru>
2021-01-01 13:21:52 +0300

    добавил на сервер статики helmet для заголовков

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { HOST, PORT } = process.env;
+
+app.use(helmet());
 
 app.use('/', express.static(path.join(__dirname, '../client/public')));
 

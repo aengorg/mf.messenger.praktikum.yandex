f00d8b0eb8b620de7c1bef61819a76e4bf8706c8
Nikita Stroganov <findoss@yandex.ru>
2021-01-19 00:38:40 +0300

    добавил утили функции

---
+++ client/source/utils/htmlToElement.ts
@@ -0,0 +1,8 @@
+export function htmlToElement(
+  html: string,
+  tagName: string = 'component',
+): Element | null {
+  const template = document.createElement(tagName);
+  template.innerHTML = html;
+  return template.firstElementChild;
+}

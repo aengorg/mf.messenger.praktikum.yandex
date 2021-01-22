1a10203f30392d4d8a405885e5c98a1ccee5b34c
Nikita Stroganov <findoss@yandex.ru>
2021-01-22 17:59:14 +0300

    рендерим тестовую страницу

--- client/source/index.ts
+++ client/source/index.ts
@@ -1,4 +1,10 @@
 import { ErrorPage } from './pages/Error/index.js';
 
-console.log('index.ts');
-new ErrorPage().render();
+const props = {
+  text: 'ошибка сервера',
+  bg: 'error500',
+};
+
+const page = new ErrorPage(props);
+const $app = document.querySelector('#app');
+$app!.appendChild(page.getElement() || document.createElement('error'));

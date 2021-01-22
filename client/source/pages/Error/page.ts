2d3e4f3619a5cec6d865f66321e3fd17951aba2b
Nikita Stroganov <findoss@yandex.ru>
2021-01-23 00:36:50 +0300

    рефакторинг страница ошибки

---
+++ client/source/pages/Error/page.ts
@@ -0,0 +1,44 @@
+import { Component, PropsComponent } from '../../core/Component/index.js';
+import template from './template.js';
+
+import { Title, PropsTitle } from '../../components/title/index.js';
+import { Link, PropsLink } from '../../components/link/index.js';
+
+export interface PropsErrorPage extends PropsComponent {
+  bg?: string;
+  title: PropsTitle;
+  link: PropsLink;
+}
+
+export class ErrorPage extends Component<PropsErrorPage> {
+  constructor(props: PropsErrorPage) {
+    super(props, {
+      link: new Link(props.link),
+      title: new Title(props.title),
+    });
+  }
+
+  public beforeCreateHandler() {}
+
+  public createdHandler() {
+    // ! BAD force update
+    setTimeout(() => {
+      this.children.title.props.text = 'Страница не найдена';
+      this.props.bg = 'error404';
+    }, 3000);
+  }
+
+  public updatedHandler() {}
+
+  public beforeUpdateHandler() {
+    return true;
+  }
+
+  public getContext() {
+    return {};
+  }
+
+  public render() {
+    return template;
+  }
+}
--- client/source/pages/Error/template.ts
+++ client/source/pages/Error/template.ts
@@ -2,6 +2,5 @@ export default `
 <div class="app app--center bg bg--img-{{= data.bg }} bg--pos-tc bg--size-l">
   {{= SLOT('title') }}
   {{= SLOT('link') }}
-  {{= data.text }}
 </div>
 `;

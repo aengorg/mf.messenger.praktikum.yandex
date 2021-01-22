bd6624ef60f5e71eeaf4e7f7308574e741112e5b
Nikita Stroganov <findoss@yandex.ru>
2021-01-22 18:01:56 +0300

    обновил страницу ошибки

--- client/source/pages/Error/index.ts
+++ client/source/pages/Error/index.ts
@@ -1,16 +1,58 @@
-import { Template } from '../../classes/Template/index.js';
+import { Component, PropsComponent } from '../../core/Component/index.js';
+import { Title } from '../../components/Title/index.js';
+import { Link } from '../../components/Link/index.js';
 import template from './template.js';
 
-console.log('Error/index.ts');
+interface PropsErrorPage extends PropsComponent {
+  bg?: string;
+  text?: string;
+}
 
-export class ErrorPage {
-  constructor() {}
+export class ErrorPage extends Component<PropsErrorPage> {
+  constructor(props: PropsErrorPage) {
+    super(props, {
+      link: new Link({
+        url: './',
+        text: 'Back to chats',
+        size: 2,
+      }),
+      title: new Title({
+        text: props.text || '',
+        level: 1,
+        size: 1,
+        color: 'red',
+      }),
+    });
+  }
 
-  render() {
-    const block = new Template(template);
-    const str = block.toHTML({ errorText: 'ошибка падла' });
+  public beforeCreateHandler() {
+    // this.children.title.props.text = this.props.text;
+  }
 
-    const $root = document.querySelector('#app');
-    if ($root) $root.innerHTML = str;
+  public createdHandler() {
+    // ! BAD !
+    setTimeout(() => {
+      this.children.title.props.text = 'Страница не найдена';
+    }, 3000);
+
+    // Good
+    setTimeout(() => {
+      this.props.text = 'Страница не найдена';
+      this.props.bg = 'error404';
+    }, 3000);
+  }
+
+  public updatedHandler() {}
+
+  public beforeUpdateHandler() {
+    return true;
+  }
+
+  public getContext() {
+    return {};
+  }
+
+  public render() {
+    return template;
   }
 }
--- client/source/pages/Error/template.ts
+++ client/source/pages/Error/template.ts
@@ -1,12 +1,7 @@
 export default `
-<div class="app app--center">
-  <img
-    src="./assets/images/backgrounds/500.svg"
-    alt="error"
-    height="500px"
-    width="500px"
-  />
-  <h1 class="title title--size-1 title--color-red">{{ errorText }}</h1>
-  <a href="./" class="link--size-2 link">Back to chats</a>
+<div class="app app--center bg bg--img-{{= data.bg }} bg--pos-tc bg--size-l">
+  {{= SLOT('title') }}
+  {{= SLOT('link') }}
+  {{= data.text }}
 </div>
 `;

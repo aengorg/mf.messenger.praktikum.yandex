d16bfa1ea764ba69ab3bee72b521fe4368355fb2
Nikita Stroganov <findoss@yandex.ru>
2021-01-24 22:42:36 +0300

    обновил утилити валидации

--- client/source/utils/validationRules/validation.ts
+++ client/source/utils/validationRules/validation.ts
@@ -1,9 +1,13 @@
-type TRule = (...args: any) => string | boolean;
+import { TRule, TErrors } from './index.js';
 
-export function validation(value: string | number, rules: TRule[]) {
+export function validation(value: string | number, rules: TRule[]): TErrors {
   const arr = rules.map((rule) => rule(value));
   if (!arr.some((v) => v !== '')) {
     return [];
   }
   return arr.filter((v) => Boolean(v));
+}
+
+export function isValid(errors: TErrors): boolean {
+  return errors.length === 0;
 }

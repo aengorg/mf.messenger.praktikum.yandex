e98d2e236905e643198bf41ac439468f556b2a43
Nikita Stroganov <findoss@yandex.ru>
2021-01-24 22:42:24 +0300

    добавил возможность получить ссылки на инпуты

--- client/source/core/DataForm/index.ts
+++ client/source/core/DataForm/index.ts
@@ -1,19 +1,39 @@
 type TCallback = (e: Event) => void;
+type TNames = string[];
 interface IData {
   [input: string]: FormDataEntryValue;
+}
+interface IInputs {
+  [id: string]: Element;
 }
 
 export class DataForm {
   private $form: HTMLFormElement | null;
+  private inputs?: IInputs;
 
-  constructor(selector: string) {
+  constructor(selector: string, names?: TNames) {
     this.$form = document.querySelector(selector);
+
+    if (names !== undefined) {
+      this.findInputs(names);
+    }
   }
 
   private submitHandler(e: Event): void {
     e.stopPropagation();
     e.preventDefault();
     console.log(this.getData());
+  }
+
+  private findInputs(names: TNames): void {
+    const inputs: IInputs = {};
+    names.forEach((name) => {
+      const input = this.$form?.querySelector(`[name=${name}]`);
+      if (input) {
+        inputs[name] = input;
+      }
+    });
+    this.inputs = inputs;
   }
 
   public addHandlerToSubmit(callback?: TCallback): void {
@@ -33,5 +53,9 @@ export class DataForm {
       for (let [key, value] of form.entries()) data[key] = value;
     }
     return data;
+  }
+
+  public getInputs(): IInputs | undefined {
+    return this.inputs;
   }
 }

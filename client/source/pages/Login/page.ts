d378ca1da2c224aa6177d473a14544dc0c0b59fd
Nikita Stroganov <findoss@yandex.ru>
2021-01-26 15:34:56 +0300

    перевел страницу логина на компонент формы

--- client/source/pages/Login/page.ts
+++ client/source/pages/Login/page.ts
@@ -1,15 +1,15 @@
-import { Component, PropsComponent } from '../../core/Component/index.js';
+import {
+  AbstractForm,
+  PropsAbstractForm,
+} from '../../components/Form/index.js';
 import template from './template.js';
-
-import { DataForm } from '../../core/DataForm/index.js';
 
 import { Title, PropsTitle } from '../../components/title/index.js';
 import { Link, PropsLink } from '../../components/link/index.js';
 import { Field, PropsField } from '../../components/field/index.js';
 import { Button, PropsButton } from '../../components/button/index.js';
-import { FormError } from '../../components/Form/formError/index.js';
 
-export interface PropsLoginPage extends PropsComponent {
+export interface PropsLoginPage extends PropsAbstractForm {
   title: PropsTitle;
   linkSignup: PropsLink;
   fieldLogin: PropsField;
@@ -17,11 +17,7 @@ export interface PropsLoginPage extends PropsComponent {
   buttonLogin: PropsButton;
 }
 
-export class LoginPage extends Component<PropsLoginPage> {
-  formSelector: string;
-  $form: HTMLFormElement | null;
-  inputsData: DataForm | null;
-
+export class LoginPage extends AbstractForm<PropsLoginPage> {
   constructor(props: PropsLoginPage) {
     super(props, {
       title: new Title(props.title),
@@ -29,43 +25,13 @@ export class LoginPage extends Component<PropsLoginPage> {
       fieldLogin: new Field(props.fieldLogin),
       fieldPassword: new Field(props.fieldPassword),
       buttonLogin: new Button(props.buttonLogin),
-      error: new FormError({}),
     });
-
-    this.formSelector = '#form-login';
-    this.$form = null;
-    this.inputsData = null;
   }
 
   public beforeCreateHandler() {}
 
   public createdHandler() {
-    this.inputsData = new DataForm(this.formSelector, ['login', 'password']);
-    this.inputsData.addHandlerToSubmit(this.submitHandler.bind(this));
-    this.$form = this.$element!.querySelector(this.formSelector);
-  }
-
-  public submitHandler(e: Event) {
-    e.preventDefault();
-    e.stopPropagation();
-    const inputs: Field[] = [
-      this.children.fieldLogin as Field,
-      this.children.fieldPassword as Field,
-    ];
-    const errors = inputs.map((input) => {
-      return input.validationHandler();
-    });
-
-    const isError = errors.some((arr) => arr.length > 0);
-
-    if (isError) {
-      this.$form?.classList.add('form--error');
-      this.children.error.props.text = 'Error field(s)';
-    } else {
-      this.$form?.classList.remove('form--error');
-      this.children.error.props.text = '';
-      console.log(this.inputsData?.getData());
-    }
+    this.initForm();
   }
 
   public updatedHandler() {}

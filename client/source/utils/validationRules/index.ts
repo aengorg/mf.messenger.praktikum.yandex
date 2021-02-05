521f503a4df270ae13279d5d23c3add3b9ffea93
Nikita Stroganov <findoss@yandex.ru>
2021-02-05 19:03:43 +0300

    исправил импорты

--- client/source/utils/validationRules/index.ts
+++ client/source/utils/validationRules/index.ts
@@ -1,3 +1,5 @@
+import { required, range, email, equalPasswords } from './validationRules.js';
+export const rules = { required, range, email, equalPasswords };
+
 export type TRule = (...args: any[]) => string;
 export type TErrors = string[];
-export { required, range, email, equalPasswords } from './validationRules.js';

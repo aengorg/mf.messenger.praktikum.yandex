b1d791a2ba96a90646e8da9ec25f70bc2fff6695
Nikita Stroganov <findoss@yandex.ru>
2021-02-10 00:18:18 +0300

    добавил транстпорт http

---
+++ client/source/core/Transport/Http/http.ts
@@ -0,0 +1,85 @@
+import { queryString } from '../../../utils/queryString/index.js';
+
+export type Data = string | { [index: string]: string | any[] } | null;
+export type Options = {
+  data?: Data;
+  getParam?: {} | null;
+  timeout?: number | null;
+  method?: string;
+  headers?: {};
+};
+
+enum Methods {
+  GET = 'GET',
+  PUT = 'PUT',
+  POST = 'POST',
+  DELETE = 'DELETE',
+  HEAD = 'HEAD',
+  PATCH = 'PATCH',
+}
+
+export class HTTPTransport {
+  options: Options = {
+    data: null,
+    getParam: null,
+    timeout: null,
+    method: '',
+    headers: {},
+  };
+
+  private apiBaseUrl: string;
+
+  constructor(url: string) {
+    this.apiBaseUrl = url;
+  }
+
+  public get = (url: string, options: Options = {}): Promise<any> => {
+    return this.request(url, { ...options, method: Methods.GET });
+  };
+
+  public post = (url: string, options: Options = {}): Promise<any> => {
+    return this.request(url, { ...options, method: Methods.POST });
+  };
+
+  public put = (url: string, options: Options = {}): Promise<any> => {
+    return this.request(url, { ...options, method: Methods.PUT });
+  };
+
+  public delete = (url: string, options: Options = {}): Promise<any> => {
+    return this.request(url, { ...options, method: Methods.DELETE });
+  };
+
+  private request = (url: string, options: Options): Promise<any> => {
+    const { data, method, timeout, getParam } = options;
+
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+
+      if (method === Methods.GET && getParam) {
+        url = `${url}${queryString(getParam)}`;
+      }
+
+      method && xhr.open(method, `${this.apiBaseUrl}${url}`);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.withCredentials = true;
+
+      xhr.onload = function () {
+        resolve(xhr);
+      };
+
+      if (timeout) {
+        setTimeout(() => xhr.abort(), timeout);
+      }
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      if (method === Methods.GET || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
+    });
+  };
+}

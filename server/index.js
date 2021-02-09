a7cf9cdb4860674839c35fbaf1e50beecf2173bf
Nikita Stroganov <findoss@yandex.ru>
2021-02-09 18:09:10 +0300

    добавил зщаголовки на сервер

--- server/index.js
+++ server/index.js
@@ -7,7 +7,17 @@ const app = express();
 
 const { HOST, PATH_STATIC, PORT } = process.env;
 
-// app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-eval'"],
+        objectSrc: ["'self'"],
+      },
+    },
+  }),
+);
 
 app.use('/', express.static(path.join(__dirname, PATH_STATIC)));
 app.get('/*', (req, res) => res.redirect('/#error404'));

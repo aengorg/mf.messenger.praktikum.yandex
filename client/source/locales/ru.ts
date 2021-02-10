a421daac66c360f4f90eed5cb3ee8793769bb18e
Nikita Stroganov <findoss@yandex.ru>
2021-02-11 00:51:33 +0300

    добапвил локализацию

---
+++ client/source/locales/ru.ts
@@ -0,0 +1,47 @@
+export const ru: { [k: string]: string } = {
+  // ? text
+  serverError: 'Ошибка сервера',
+  pageNotFound: 'Страница не найдена',
+  backToChats: 'Вернуться к чатам',
+  welcome: 'Привет!',
+  join: 'Подключайся!',
+  editProfile: 'Редактировать профиль',
+  again: 'еще раз',
+  ok: 'Ok',
+  changePassword: 'Изменить пароль',
+  profilePhoto: 'Фото профиля',
+
+  // ? button
+  cancel: 'Отмена',
+  save: 'Сохранить',
+  removePhoto: 'Удалить фото',
+  myProfile: 'Мой профиль',
+  logIn: 'Войти',
+  logout: 'Выйти',
+  registration: 'Регистрация',
+  uploadAnImage: 'Загрузить фото',
+
+  // ? fields
+  login: 'Логин',
+  email: 'Почта',
+  firstName: 'Имя',
+  secondName: 'Фамилия',
+  chatName: 'Ник в чате',
+  password: 'Пароль',
+  phone: 'Телефон',
+  oldPassword: 'Старый пароль',
+  newPassword: 'Новый пароль',
+
+  // ? validation rules
+  ruleRequired: 'Это поле обязательно',
+  ruleRangeSmall: 'Значение короче чем',
+  ruleRangeBig: 'Значение длинее чем',
+  ruleEmail: 'Формат почты не правильный',
+  rulePhone: 'Формат телефона не правильный',
+  ruleEqualPasswords: 'Пароли не совпадают',
+
+  // ? Responses
+  'user already in system': 'Пользователь уже в системе',
+  'Login or password is incorrect': 'Логин или пароль не верные',
+  'Cookie is not valid': 'Ошибка, не возможное действие #001',
+};

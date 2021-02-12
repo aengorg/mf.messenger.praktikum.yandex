1708a1908602a6be338ceabd4eb10231e4cb8d69
Nikita Stroganov <findoss@yandex.ru>
2021-02-12 17:34:40 +0300

    добавил пути для апи пользователя

--- client/source/constants/index.ts
+++ client/source/constants/index.ts
@@ -2,3 +2,4 @@ export const TAG_SLOT: string = 'component-slot';
 
 export const API_HOST: string = 'https://ya-praktikum.tech/api/v2';
 export const API_AUTH: string = '/auth';
+export const API_USER: string = '/user';

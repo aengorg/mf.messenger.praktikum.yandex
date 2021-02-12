3876935e062147a0cba943b0c5c39249717e5c59
Nikita Stroganov <findoss@yandex.ru>
2021-02-12 17:33:44 +0300

    дополнил типы api

--- client/source/api/types.ts
+++ client/source/api/types.ts
@@ -1,33 +1,66 @@
-type TypeId = { id: number };
-type TypeFirstName = { first_name: string };
-type TypeSecondName = { second_name: string };
-type TypeLogin = { login: string };
-type TypeEmail = { email: string };
-type TypePassword = { password: string };
-type TypePhone = { phone: string };
-
-type TypeFullName = TypeFirstName & TypeSecondName;
-type TypeUserData = TypeFullName & TypeLogin & TypeEmail & TypePhone;
-
+// * Response
 export type TypeEmptyResponse = {};
-
 export type TypeBadResponse = {
   reason: string;
 };
-
 export type TypeGoodResponse = {
   message: string;
 };
 
-export type TypeUser = TypeId &
-  TypeUserData & {
-    display_name: string;
-    avatar: string;
-  };
+// * SignUp
+export type TypeSignUpRequest = {
+  first_name: string;
+  second_name: string;
+  login: string;
+  email: string;
+  password: string;
+  phone: string;
+};
+export type TypeSignUpResponse = { id: number };
+export type TypeSignUpForm = TypeSignUpRequest & { repeat_password: string };
 
-export type TypeSignInRequest = TypeLogin & TypePassword;
+// * Signin
+export type TypeSignInRequest = {
+  login: string;
+  password: string;
+};
 export type TypeSignInResponse = TypeEmptyResponse;
 
-export type TypeSignUpRequest = TypeUserData & TypePassword;
-export type TypeSignUpResponse = TypeId;
-export type TypeSignUpForm = TypeSignUpRequest & { repeat_password: string };
+// * User
+export type TypeUserResponse = {
+  id: number;
+  first_name: string;
+  second_name: string;
+  display_name: string | null;
+  login: string;
+  email: string;
+  phone: string;
+  avatar: string | null;
+};
+
+export type TypeUserPasswordRequest = {
+  oldPassword: string;
+  newPassword: string;
+};
+export type TypeUserPasswordResponse = TypeGoodResponse;
+
+export type TypeUserProfileRequest = {
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  login: string;
+  email: string;
+  phone: string;
+};
+
+export type TypeUserProfileResponse = {
+  first_name: string;
+  second_name: string;
+  display_name: string;
+  login: string;
+  email: string;
+};
+
+export type TypeUserLogin = {
+  login: string;
+};

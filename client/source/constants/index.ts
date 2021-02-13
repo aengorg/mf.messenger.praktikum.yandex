28251a7fa2ffc88fa79c385868281003f506085d
Nikita Stroganov <findoss@yandex.ru>
2021-02-13 15:47:28 +0300

    рефакторинг констант

--- client/source/constants/index.ts
+++ client/source/constants/index.ts
@@ -1,5 +1,6 @@
 export const TAG_SLOT: string = 'component-slot';
 
-export const API_HOST: string = 'https://ya-praktikum.tech/api/v2';
+export const HOST: string = 'https://ya-praktikum.tech';
+export const API_HOST: string = `${HOST}/api/v2`;
 export const API_AUTH: string = '/auth';
 export const API_USER: string = '/user';

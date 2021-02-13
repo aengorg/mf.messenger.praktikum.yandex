a184d115b7a881cbbdff07e8cec565674049142f
Nikita Stroganov <findoss@yandex.ru>
2021-02-13 15:25:10 +0300

    обновил типы

--- client/source/services/auth.ts
+++ client/source/services/auth.ts
@@ -1,8 +1,10 @@
 import { ApiAuth } from '../api/auth.js';
 import {
+  TypeGoodResponse,
   TypeSignInRequest,
   TypeSignUpForm,
   TypeSignUpResponse,
+  TypeUserResponse,
 } from '../api/types.js';
 import { t } from '../locales/index.js';
 
@@ -14,7 +16,7 @@ export class AuthService {
   }
 
   public signIn(data: TypeSignInRequest) {
-    return new Promise((resolve, reject) => {
+    return new Promise<TypeGoodResponse>((resolve, reject) => {
       this.api.signIn(data).then((res) => {
         if (res.status === 200) {
           resolve({ message: t['ok'] });
@@ -27,27 +29,42 @@ export class AuthService {
   }
 
   public signUp(data: TypeSignUpForm) {
-    return new Promise(
-      (resolve: (value: TypeSignUpResponse) => any, reject) => {
-        this.api.signUp(data).then((res) => {
-          if (res.status === 200) {
-            resolve(JSON.parse(res.response));
-          } else {
-            const errorStr = JSON.parse(res.response).reason;
-            reject(t[errorStr]);
-          }
-        });
-      },
-    );
+    return new Promise<TypeSignUpResponse>((resolve, reject) => {
+      this.api.signUp(data).then((res) => {
+        if (res.status === 200) {
+          resolve(JSON.parse(res.response));
+        } else {
+          const errorStr = JSON.parse(res.response).reason;
+          reject(t[errorStr]);
+        }
+      });
+    });
   }
 
   public logout() {
-    return new Promise((resolve, reject) => {
+    return new Promise<TypeGoodResponse>((resolve, reject) => {
       this.api.logout().then((res) => {
         if (res.status === 200) {
           resolve({ message: t['ok'] });
         } else {
           const errorStr = JSON.parse(res.response).reason;
+          reject(t[errorStr]);
+        }
+      });
+    });
+  }
+
+  public getUser() {
+    return new Promise<TypeUserResponse>((resolve, reject) => {
+      this.api.getUser().then((res) => {
+        if (res.status === 200) {
+          const userData: TypeUserResponse = JSON.parse(res.response);
+          resolve(userData);
+        } else if (res.status === 401) {
+          reject(t['ErrorUnauthorized']);
+        } else {
+          const errorStr = JSON.parse(res.response).reason;
+
           reject(t[errorStr]);
         }
       });

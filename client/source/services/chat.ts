9cfa75b141c37765a4e1f9f1c5e11c17acf73842
Nikita Stroganov <findoss@yandex.ru>
2021-02-15 00:28:51 +0300

    добавил сервис чата

---
+++ client/source/services/chat.ts
@@ -0,0 +1,44 @@
+import { ApiChat } from '../api/chat.js';
+import {
+  TypeChatRequest,
+  TypeChatsResponse,
+  TypeChatResponse,
+} from '../api/types.js';
+import { t } from '../locales/index.js';
+
+export class ChatService {
+  api: ApiChat;
+
+  constructor() {
+    this.api = new ApiChat();
+  }
+
+  public addChat(data: TypeChatRequest) {
+    return new Promise<TypeChatResponse>((resolve, reject) => {
+      this.api.addChat(data).then((res) => {
+        if (res.status === 200) {
+          resolve({ message: t['ok'] });
+        } else {
+          const errorStr = JSON.parse(res.response).reason;
+          reject(t[errorStr]);
+        }
+      });
+    });
+  }
+
+  public getChats() {
+    return new Promise<TypeChatsResponse>((resolve, reject) => {
+      this.api.getChats().then((res) => {
+        if (res.status === 200) {
+          const chats: TypeChatsResponse = JSON.parse(res.response);
+          resolve(chats);
+        } else {
+          const errorStr = JSON.parse(res.response).reason;
+          reject(t[errorStr]);
+        }
+      });
+    });
+  }
+}
+
+export const chatService = new ChatService();

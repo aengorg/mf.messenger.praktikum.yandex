c036844b051a7d209e25ef2b6e457d831d047383
Nikita Stroganov <findoss@yandex.ru>
2021-02-17 13:19:48 +0300

    обновил локализацию

--- client/source/locales/ru.ts
+++ client/source/locales/ru.ts
@@ -7,12 +7,14 @@ export const ru: { [k: string]: string } = {
   join: 'Подключайся!',
   editProfile: 'Редактировать профиль',
   again: 'еще раз',
-  ok: 'Ok',
   changePassword: 'Изменить пароль',
   profilePhoto: 'Фото профиля',
   textAddFirstChat: 'Добавьте свой первый чат',
   createChat: 'Создать чат',
   textSelectChat: 'Выберите чат',
+  addUserChat: 'Добавить в чат',
+  usersChat: 'Участники чата',
+  listEmpty: 'Список пуст',
 
   // ? button
   cancel: 'Отмена',
@@ -25,6 +27,7 @@ export const ru: { [k: string]: string } = {
   registration: 'Регистрация',
   uploadAnImagePhoto: 'Загрузить фото',
   create: 'Создать',
+  close: 'Закрыть',
 
   // ? fields
   login: 'Логин',
@@ -47,12 +50,17 @@ export const ru: { [k: string]: string } = {
   ruleEqualPasswords: 'Пароли не совпадают',
 
   // ? Responses
+  ok: 'Ok',
+  successAddUserChat: 'Пользователь успешно добавлен',
+  successDeleteUserChat: 'Пользователь успешно удален',
   'user already in system': 'Пользователь уже в системе',
   'Login or password is incorrect': 'Логин или пароль не верные',
   'Cookie is not valid': 'Ошибка #001',
   'No chat': 'Такого чата нет',
+  'Wrong json format': 'Неверный формат данных (json)',
   errorUnauthorized: 'Нет доступа, необходимо войти',
   errorPasswordIsIncorrect: 'Текущий пароль указан не верно',
   errorDefaultForm: 'Ошибка в форме, проверьте все поля',
   errorUploadAvatar: 'Ошибка загрузки аватара #002',
+  userNotFound: 'Пользователь не найден',
 };

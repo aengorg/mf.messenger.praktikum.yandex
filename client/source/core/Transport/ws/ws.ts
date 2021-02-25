1672a324bd22086138ec7d9a9fd4fbbc030939ba
Nikita Stroganov <findoss@yandex.ru>
2021-02-26 00:23:33 +0300

    добавил новый транспорт вебсокеты

---
+++ client/source/core/Transport/ws/ws.ts
@@ -0,0 +1,25 @@
+export class WSTransport {
+  private apiBaseUrl: string;
+  public socket: WebSocket | null;
+
+  constructor(url: string) {
+    this.apiBaseUrl = url;
+    this.socket = null;
+  }
+
+  public open(chanel: string) {
+    this.socket = new WebSocket(`${this.apiBaseUrl}${chanel}`);
+  }
+
+  public send(message: string) {
+    if (this.socket !== null) {
+      this.socket.send(message);
+    }
+  }
+
+  public close() {
+    if (this.socket !== null) {
+      this.socket.close();
+    }
+  }
+}
